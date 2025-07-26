18fd613ecce5d0b5ff0ff359e6bacfc5f3596ddb
Christopher Joule Xenon <chris@grimtech.net>
2025-07-25 19:26:40 -0800

    fix logger

--- src/util/logger.js
+++ src/util/logger.js
@@ -14,15 +14,21 @@ if (env.NODE_ENV === 'test') {
     }
 }
 
+const isProd = env.NODE_ENV === 'production'
 const logger = pino({
     level: env.LOG_LEVEL,
-    transport: {
-        target: env.NODE_ENV === 'production' ? 'pino' : 'pino-pretty',
-        options: {
-            colorize: env.NODE_ENV === 'production' ? false : true,
-        }
-    },
-    ...(hooks ? { hooks } : {})
+    ...(isProd
+        ? {} // no transport needed in production if logging to JSON
+        : {
+            transport: {
+                target: 'pino-pretty',
+                options: {
+                    colorize: true,
+                    sync: true, // avoid using thread-stream in test/dev
+                },
+            },
+        }),
 })
+
 
 export default logger
